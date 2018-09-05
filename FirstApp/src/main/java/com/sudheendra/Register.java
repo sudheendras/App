72b29153358c28cd453933e84842293a330ad1cc
Sudheendra <sudheendra.satrasala@gmail.com>
2018-09-05 22:16:41 +0100

    First App committed

---
+++ FirstApp/src/main/java/com/sudheendra/Register.java
@@ -0,0 +1,21 @@
+package com.sudheendra;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Register extends HttpServlet {
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String n = request.getParameter("userName");
+		String p = request.getParameter("userPass");
+		String e = request.getParameter("userEmail");
+		String c = request.getParameter("userCountry");
+		out.write(n+"\t"+p+"\t"+e+"\t"+c);
+	}
+
+}
